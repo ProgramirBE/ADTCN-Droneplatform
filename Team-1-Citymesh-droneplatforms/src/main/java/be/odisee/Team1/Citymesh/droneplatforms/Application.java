21edb66aa6194114b73280095167276ee5910c0a
andreas-giron <146851001+andreas-giron@users.noreply.github.com>
2025-10-09 10:47:31 +0200

    setup spring

---
+++ Team-1-Citymesh-droneplatforms/src/main/java/be/odisee/Team1/Citymesh/droneplatforms/Application.java
@@ -0,0 +1,13 @@
+package be.odisee.Team1.Citymesh.droneplatforms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ Team-1-Citymesh-droneplatforms/src/test/java/be/odisee/Team1/Citymesh/droneplatforms/ApplicationTests.java
@@ -0,0 +1,13 @@
+package be.odisee.Team1.Citymesh.droneplatforms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
